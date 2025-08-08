0e8767d548d2f9c756dcfcce6c7abe975218cbaf
Sergio Mondragon <sergiomondragondev@gmail.com>
2025-08-07 22:17:05 -0500

    Add contact and professionals pages with associated scripts and styles; include new images and icons

---
+++ project/scripts/home.js
@@ -0,0 +1,86 @@
+professionalValues = [
+    {
+        id: 1,
+        svgIcon: "images/self-reliance-icon.svg",
+        name: "Self-Reliance",
+        description: "Empowering individuals to grow through skill and effort."
+    },
+    {
+        id: 2,
+        svgIcon: "images/trust-icon.svg",
+        name: "Trust & Integrity",
+        description: "Honest work that builds respect and lasting relationships."
+    },
+    {
+        id: 3,
+        svgIcon: "images/growth-icon.svg",
+        name: "Community Groth",
+        description: "Supporting locals to strengthen and uplift our neighborhood."
+    }
+]
+
+services = [
+    {
+        id:1,
+        imageSrc: "images/electricians-800-530.webp",
+        name: "Electricians"
+    },
+    {
+        id:2,
+        imageSrc: "images/painters-800-530.webp",
+        name: "Painters"
+    },
+    {
+        id:3,
+        imageSrc: "images/taxi-drivers-800-530.webp",
+        name: "Taxi Drivers"
+    },
+]
+
+function createValueCard(proffesionalValue){
+    const valueListDiv = document.getElementById("value-list")
+    const valueCardDiv = document.createElement("div");
+    valueCardDiv.classList.add("card")
+    const svgIcon = document.createElement("img");
+    svgIcon.setAttribute("alt",`${proffesionalValue.name} Icon`);
+    svgIcon.setAttribute("src",proffesionalValue.svgIcon);
+    svgIcon.setAttribute("width",96);
+    const title = document.createElement("h3");
+    title.textContent = proffesionalValue.name;
+    const description = document.createElement("p");
+    description.textContent = proffesionalValue.description;
+    valueCardDiv.appendChild(svgIcon);
+    valueCardDiv.appendChild(title);
+    valueCardDiv.appendChild(description);
+    valueListDiv.append(valueCardDiv);
+}
+
+function createServiceCard(service){
+    const listDiv = document.getElementById("services-list")
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card")
+    const img = document.createElement("img");
+    img.setAttribute("alt",`${service.name} Card Image`);
+    img.setAttribute("src",service.imageSrc);
+    img.setAttribute("width",250);
+    img.setAttribute("loading","lazy");
+    const title = document.createElement("h3");
+    title.textContent = service.name;
+    const actionButton = document.createElement("button");
+    actionButton.textContent = "Explore";
+    actionButton.classList.add("service-button")
+
+    actionButton.addEventListener('click', () => {
+        window.location.href = "professionals.html";
+        localStorage.setItem("service",service.name);
+    });
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(title);
+    cardDiv.appendChild(actionButton);
+    listDiv.append(cardDiv);
+
+
+}
+
+professionalValues.forEach(element => createValueCard(element));
+services.forEach(service => createServiceCard(service));
---
+++ project/scripts/professionals.js
@@ -0,0 +1,135 @@
+services = [
+    {
+        id: 0,
+        name: "All"
+    },
+    {
+        id: 1,
+        name: "Electricians"
+    },
+    {
+        id: 2,
+        name: "Painters"
+    },
+    {
+        id: 3,
+        name: "Taxi Drivers"
+    },
+]
+
+professionals = [
+    {
+        id: 1,
+        imageSrc: "images/carlos-brenis-400-400.webp",
+        name: "Carlos Brenis",
+        phone: "+51 945123422",
+        service: "Painters",
+        description: "Painter with more than 20 years of experience. Specializes in residential and commercial projects, delivering high-quality finishes and attention to detail. Reliable, punctual, and customer-focused."
+    },
+    {
+        id: 2,
+        imageSrc: "images/napoleon-400-400.webp",
+        name: "Napoleon Monteza",
+        phone: "+51 945123422",
+        service: "Electricians",
+        description: "Electrician with over 15 years of experience. Expert in wiring, repairs, and installations. Known for safety, efficiency, and excellent troubleshooting skills. Trusted by many satisfied clients."
+    },
+    {
+        id: 3,
+        imageSrc: "images/jhon-zavaleta-400-400.webp",
+        name: "Jhon Zavaleta",
+        phone: "+51 945123422",
+        service: "Taxi Drivers",
+        description: "Taxi driver with 5+ years of experience. Provides safe, comfortable rides. Friendly, punctual, and knowledgeable about city routes. Committed to excellent customer service."
+    },
+    {
+        id: 4,
+        imageSrc: "images/eliberto-sanchez-400-400.webp",
+        name: "Eliberto Sanchez",
+        phone: "+51 945123422",
+        service: "Taxi Drivers",
+        description: "Taxi driver with 30 years of experience. Reliable, courteous, and familiar with all city areas. Ensures timely arrivals and a pleasant travel experience for every passenger."
+    },
+]
+
+function createServiceFilter(service) {
+    const serviceList = document.getElementById("available-services");
+    const serviceLi = document.createElement("li");
+    const serviceFilter = document.createElement("a");
+    serviceFilter.innerText = service.name;
+    serviceFilter.id = `service-${service.name}`
+    serviceFilter.addEventListener("click", () => filterByService(service.name));
+
+    serviceLi.appendChild(serviceFilter);
+    serviceList.appendChild(serviceLi);
+}
+
+function createProfessionalCard(professional) {
+    const listDiv = document.getElementById("professionals-list")
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card")
+    const img = document.createElement("img");
+    img.setAttribute("alt", `${professional.name} Card Image`);
+    img.setAttribute("src", professional.imageSrc);
+    img.setAttribute("width", 150);
+    img.setAttribute("loading", "lazy");
+    const title = document.createElement("h3");
+    title.textContent = professional.name;
+    const paragraph = document.createElement("p");
+    paragraph.textContent = professional.description;
+    const actionButton = document.createElement("a");
+    actionButton.textContent = "Contact him";
+    actionButton.href = `tel:${professional.phone}`
+    actionButton.classList.add("professional-button")
+    // Div for phone
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("phone-div");
+    const phoneP = document.createElement("p");
+    phoneP.textContent = professional.phone;
+    const svgIcon = document.createElement("img");
+    svgIcon.src = "images/phone-icon.svg";
+    phoneDiv.appendChild(phoneP);
+    phoneDiv.appendChild(svgIcon);
+
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(title);
+    cardDiv.appendChild(paragraph);
+    cardDiv.appendChild(phoneDiv);
+    cardDiv.appendChild(actionButton);
+    listDiv.append(cardDiv);
+}
+
+function selectService(serviceName){
+
+    const selectedService = document.querySelector(".service-selected");
+    if (selectedService){
+        selectedService.classList.remove("service-selected");
+    }
+    const serviceToSelect = document.getElementById(`service-${serviceName}`)
+    serviceToSelect.classList.add("service-selected");
+}
+
+function filterByService(serviceName) {
+    let filteredProfessionals = professionals;
+    if (serviceName !== "All") {
+        filteredProfessionals = professionals.filter(
+            (professional) => professional.service === serviceName);
+    }
+    const listDiv = document.getElementById("professionals-list")
+    listDiv.innerHTML = "";
+    filteredProfessionals.forEach(professional => createProfessionalCard(professional));
+    localStorage.setItem("service",serviceName);
+    selectService(serviceName);
+}
+
+function preloadProfessionalsByService() {
+    const selectedService = localStorage.getItem("service");
+    if (selectedService) {
+        filterByService(selectedService);
+    } else {
+        filterByService("All");
+    }
+}
+
+services.forEach(service => createServiceFilter(service));
+preloadProfessionalsByService();
